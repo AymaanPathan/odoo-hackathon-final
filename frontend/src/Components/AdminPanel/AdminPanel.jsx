63ed20d629bc066cbb765a2a8760148583f4c58a
Aymaan_2003 <118355098+AymaanPathan@users.noreply.github.com>
2024-07-14 16:24:14 +0530

    Update AdminPanel.jsx

--- frontend/src/Components/AdminPanel/AdminPanel.jsx
+++ frontend/src/Components/AdminPanel/AdminPanel.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import toast from "react-hot-toast";
 import MainContent from "./MainContent";
 
 function AdminPanel() {
+  useEffect(() => {
+    const role = localStorage.getItem("role");
+    if (role && role === "User") {
+      toast.error("You are not allowed to access the admin panel.");
+      window.location.href = "/";
+    }
+  }, []);
+
   return (
     <div className="flex h-full">
       <MainContent />
